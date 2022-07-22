65bf72ed0956d39f1036875519f88d0550b2adab
Emanuele <103491766+DezVibez@users.noreply.github.com>
2022-07-22 17:44:23 +0200

    main arcodato

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,5 +1,15 @@
 <template>
   <div id="main">
+
+    <div class="container">
+      <ul>
+        <li><img src="../assets/img/buy-comics-digital-comics.png" alt=""><span>digital comics</span></li>
+        <li><img src="../assets/img/buy-comics-merchandise.png" alt=""><span>digital comics</span></li>
+        <li><img src="../assets/img/buy-comics-shop-locator.png" alt=""><span>digital comics</span></li>
+        <li><img src="../assets/img/buy-comics-subscriptions.png" alt=""><span>digital comics</span></li>
+        <li><img src="../assets/img/buy-dc-power-visa.svg" alt=""><span>digital comics</span></li>
+      </ul>
+    </div>
     
   </div>
 </template>
@@ -12,8 +22,32 @@ export default {
 <style lang="scss">
 
 #main {
-    height: 130px;
+    padding:14px;
     background-color: rgb(0,130,249);
-    display: flex;
 }
+
+#main img {
+  width: 2.5rem;
+}
+
+#main ul, #main li{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+}
+
+#main span{
+  color: white;
+  font-size: 14px;
+  font-weight: 100;
+  height: 100%;
+  margin-left: 10px
+}
+
+#main .container {
+    max-width: 70%;
+    height: auto;
+    margin: 0 auto;
+  }
 </style>
--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,7 +1,11 @@
 <template>
+
   <div id="products">
-    
+    <div class="container">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda molestias quisquam inventore officia, asperiores architecto facere, excepturi vel iusto eos natus consectetur, recusandae consequatur voluptatum. Dignissimos vel rerum quam ratione?
+    </div>
   </div>
+
 </template>
 
 <script>
@@ -14,6 +18,15 @@ export default {
 #products {
     height: 90px;
     background-color: rgb(28,28,28);
+  }
+
+  #products .container {
+    max-width: 70%;
+    height: auto;
+    margin: 0 auto;
     display: flex;
-    }
+    justify-content: space-between;
+    align-items: center;
+  }
 </style>
+
